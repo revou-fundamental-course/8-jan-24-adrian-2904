f06bdf56c6c49599b058926a7521bbc94314c51b
adrian-2904 <156438279+adrian-2904@users.noreply.github.com>
2024-01-17 01:01:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 //Luas Persegi//
 function jumlah() {
     var s = document.getElementById(`squareSide`).value;
+    var a = " X "
+    var Result =s + a + s
     var hasil = s*s
+    document.getElementById("oke").innerHTML = Result;
     document.getElementById(`Luas`).innerHTML = hasil;
 }
 
 //Keliling Persegi//
 function Result() {
     var s = document.getElementById(`Keliling`).value;
+    var a = " X "
+    var b = 4
+    var Result = b + a + s 
     var hasil = 4*s
+    document.getElementById(`oke1`).innerHTML = Result
     document.getElementById(`kel`).innerHTML = hasil;
 }
